e5273e5cd771ee46912ace5c7a1e055170bb6c25
Nabila Putri Karni <bila7634@gmail.com>
2025-03-17 16:40:55 +0800

    Finish posttest 2

---
+++ kelas/pertemuan-1.cpp
@@ -0,0 +1,56 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+// cout << "Hello, World! ";
+// cout << "Belajar C++";
+// return 0;
+// }
+
+
+// int main() {
+// int a, b, hasil;
+// cout << "Masukkan nilai a: ";
+// cin >> a;
+// cout << "Masukkan nilai b: ";
+// cin >> b;
+// // Penjumlahan
+// hasil = a + b;
+// cout << "Hasil penjumlahan: " << hasil << endl;
+// // Pengurangan
+// hasil = a - b;
+// cout << "Hasil pengurangan: " << hasil << endl;
+// // Perkalian
+// hasil = a * b;
+// cout << "Hasil perkalian: " << hasil << endl;
+// // Pembagian
+// hasil = a / b;
+// cout << "Hasil pembagian: " << hasil << endl;
+// // Modulus
+// hasil = a % b;
+// cout << "Hasil modulus: " << hasil << endl;
+// return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     bool malas = false;
+//     if (malas) {
+//     cout << "Saya scroll fesbuk";
+//     } else {
+//     cout << "Saya belajar";
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int main() {
+    int i = 0;
+    while (i < 5) {
+        cout << i << endl;
+        i++;
+    }
+return 0;
+}
---
+++ kelas/pertemuan-2.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(int argc, char const *argv[])
+// {
+//     int number[3] = {1, 3, 5};
+
+//     for (int i = 0; i < 3; i++)
+//     {
+//         cout << "nilainya : " << i << endl;
+//     }
+//     number[2] = 10
+// }
+// int totalByte = sizeof(number);
+// int panjangArray = sizeof(number) / sizeof(number[0]);
+
+// // for (int i = 0; i < panjangArray; i++)
+// // {
+
+//     for (int i : number)
+//     {
+//         cout << "Nilai array : " << endl;
+//     }
+//          cout << "Nilai Array : " << number[1];
+
+// }
+
+// cout << "Versi c++ saya : " << __cplusplus;
+
+// return 0;
