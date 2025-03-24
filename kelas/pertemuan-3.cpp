f46b0b268811f4a5236760f8140bd3116a1df9de
Nabila Putri Karni <bila7634@gmail.com>
2025-03-24 21:44:10 +0800

    Finish Post test 3

---
+++ kelas/pertemuan-3.cpp
@@ -0,0 +1,49 @@
+// #include <iostream>
+// using namespace std;
+
+// struct Tanggal
+// {
+//     int hari;
+//     int bulan;
+//     int tahun;
+// };
+
+// struct Mahasiswa
+// {
+//     string nama;
+//     int nim;
+//     string gender;
+//     Tanggal tanggallahir;
+// };
+
+// int main()
+// {
+
+//     Mahasiswa mhsbaru;
+//     Mahasiswa mhslama;
+
+//     mhsbaru.nama = "bila";
+//     mhsbaru.nim = 41;
+//     mhsbaru.gender = "pr";
+//     mhsbaru.tanggallahir.hari = 2;
+//     mhsbaru.tanggallahir.bulan = 8;
+//     mhsbaru.tanggallahir.tahun = 2005;
+
+//     cout << mhsbaru.nama << endl;
+//     cout << mhsbaru.nim << endl;
+//     cout << mhsbaru.gender << endl;
+//     cout << "Tanggal Lahir: " << mhsbaru.tanggallahir.hari << "/" << mhsbaru.tanggallahir.bulan << "/" << mhsbaru.tanggallahir.tahun << endl;
+
+//     Mahasiswa mhsAbadi[5];
+//     mhsAbadi[0].nama = "bila";
+//     mhsAbadi[1].nama = "triya";
+//     mhsAbadi[2].nama = "andra";
+//     mhsAbadi[3].nama = "zira";
+
+//     for (int i = 0; i < size(mhsAbadi); i++)
+//     {
+//         cout << mhsAbadi[i].nama << endl;
+//     }
+
+//     return 0;
+// }
